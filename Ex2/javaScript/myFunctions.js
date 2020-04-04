d28a23a6a42ccda3789106d903ed811742be085f
Beata <20beata17@gmail.com>
2020-04-04 18:33:53 +0200

    addCell done

--- Ex2/javaScript/myFunctions.js
+++ Ex2/javaScript/myFunctions.js
@@ -1,16 +1,31 @@
 var addButton = document.getElementById("addButton");
-var myTable = document.getElementById("mainTable");
+var myTable = document.getElementById("myTable");
 
 
 addButton.setAttribute('onclick', 'addCell()');
 
 function addCell(){
 
-    let newRow = document.createElement('tr');
-    let newCell = documet.createElement('td');
+    var tableRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+    var newRow   = tableRef.insertRow();
+    var author  = newRow.insertCell(0);
+    var title  = newRow.insertCell(1);
+    var third  = newRow.insertCell(2);
 
-    myTable.setAttribute('background-color', 'green');
+    //var newText  = document.createTextNode('New row');
+    author.append(document.createElement('input'));
+    title.append(document.createElement('input'));
 
+    var saveButton = document.createElement('button');
+    saveButton.innerHTML = "Save";
+
+    third.appendChild(saveButton);
+
+    var removeButton = document.createElement('button');
+    removeButton.innerHTML = "Remove";
+
+    third.appendChild(removeButton);
+    
 }
 
 function helloWorld(){
