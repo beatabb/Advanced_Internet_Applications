f34acc1a1180a024da1b468d242c4ab396f8ccc5
Beata <20beata17@gmail.com>
2020-05-09 18:54:55 +0200

    with products

--- Ex5_(Nodejs)/myStore/routes/index.js
+++ Ex5_(Nodejs)/myStore/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Parrots world' });
+  res.render('shop/index', { title: 'Parrots world' });
 });
 
 module.exports = router;
