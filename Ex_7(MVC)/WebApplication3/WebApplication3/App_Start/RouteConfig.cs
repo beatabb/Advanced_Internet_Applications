142a5de9478541867fc532ad1e7ea354f952cba5
Beata <rrrr@vp.pl>
2020-06-04 01:17:59 +0200

    MVC task started

---
+++ Ex_7(MVC)/WebApplication3/WebApplication3/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication3
+{
+    public class RouteConfig
+    {
+        // służy do przechowywania reguł mapujących adresy na akcje kontrolerów
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // dodaje pojedynczą regułę do kolekcji
+            routes.MapRoute(
+                // name - służy do podania nazwy reguły
+                name: "Default",
+                // url - przekazuje wzorzec adresu
+                url: "{controller}/{action}/{id}",
+                // defaults -  zawiera przypisanie wartości domyślnych
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
